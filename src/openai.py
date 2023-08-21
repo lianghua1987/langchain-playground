c6eb6290d7f4c35ac972ddeff7ba16fd5fa12e34
lianghua1987 <hua_liang@mckinsey.com>
2023-08-21 12:44:26 +0800

    Add hugging face

---
+++ src/openai.py
@@ -0,0 +1,18 @@
+from dotenv import dotenv_values
+from langchain.llms import OpenAI
+
+print('Welcome to LangChain Playground - OpenAI')
+config = dotenv_values(".env")
+
+# temperature from 0 to 1, 0 implies deterministic response, 1 gives creative response
+# default to text-davinci-003
+llm = OpenAI(openai_api_key=config["OPENAI_API_KEY"], temperature=0.9)
+prompt = """
+    What would a good catchy and fancy name be for a github project?
+"""
+
+# print(llm(prompt))
+
+result = llm.generate([prompt] * 5)
+for name in result.generations:
+    print(name[0].text)
